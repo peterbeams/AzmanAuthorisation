65718e670f9c795dfefdddfe0ac5e227e5da7be0
Peter Beams <peter.beams@googlemail.com>
2011-12-17 17:23:35 +0000

    can get a list of operations

--- Lockdown/Lockdown/AuthorizationStore.cs
+++ Lockdown/Lockdown/AuthorizationStore.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using AZROLESLib;
 
 namespace Lockdown
 {
@@ -11,14 +14,43 @@ namespace Lockdown
 
     public class AuthorizationStore
     {
+        private readonly IAzApplication _application;
+
         public AuthorizationStore(string connectionString)
         {
-            throw new NotImplementedException();
+            var store = new AzAuthorizationStore();
+            store.Initialize(0, connectionString, null);
+            _application = store.OpenApplication("MyApp", null);
         }
 
         public IEnumerable<Operation> GetOperations()
         {
-            return null;
+            var azOps = _application.Operations;
+            var enumerator = azOps.GetEnumerator();
+            var ops = new List<Operation>();
+
+            try
+            {
+                while (enumerator.MoveNext())
+                {
+                    var o = (IAzOperation2)enumerator.Current;
+                    ops.Add(new Operation
+                    {
+                        Name = o.Name,
+                        Id = o.OperationID
+                    });
+
+                    Marshal.FinalReleaseComObject(o);
+                }
+            }
+            finally
+            {
+                var adapter = (ICustomAdapter)enumerator;
+                Marshal.ReleaseComObject(adapter.GetUnderlyingObject());
+                Marshal.FinalReleaseComObject(azOps);
+            }
+            
+            return ops;
         }
     }
 }
