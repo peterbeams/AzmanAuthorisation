4b9536896bce6e0619a9ef06fb6335fe1475920d
Peter Beams <peter.beams@googlemail.com>
2012-02-07 21:34:09 +0000

    adding uat for Operation Discovery and test MVC app for #31

---
+++ Lockdown/Achme.MyApp/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Achme.MyApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Lockdown/Achme.MyApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Achme.MyApp.MvcApplication" Language="C#" %>
