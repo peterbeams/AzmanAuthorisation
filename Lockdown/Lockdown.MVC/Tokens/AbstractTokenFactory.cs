9fcfa48355207b0c4b5473efd8156d36fdd2772f
Peter Beams <peter.beams@googlemail.com>
2012-03-04 19:12:20 +0000

    adding to token store so that authorized opeations are cleared down on logout

---
+++ Lockdown/Lockdown.MVC/Tokens/AbstractTokenFactory.cs
@@ -0,0 +1,14 @@
+﻿using Lockdown.Messages.Data;
+
+namespace Lockdown.MVC.Tokens
+{
+    public abstract class AbstractTokenFactory : ITokenFactory
+    {
+        public abstract UserToken GetCurrent();
+
+        public void Clear()
+        {
+            OperationStore.Clear();
+        }
+    }
+}
