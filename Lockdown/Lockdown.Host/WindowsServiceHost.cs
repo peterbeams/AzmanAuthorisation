183e16dc5c569eddeaaf71ebdda7466b425360b0
Peter Beams <peter.beams@googlemail.com>
2012-03-04 21:04:37 +0000

    prepping host to run as windows service

---
+++ Lockdown/Lockdown.Host/WindowsServiceHost.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceProcess;
+
+namespace Lockdown.Host
+{
+    public class WindowsServiceHost : ServiceBase
+    {
+        private AuthzServiceHost _host;
+
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+            _host = new AuthzServiceHost();
+            _host.Start();
+        }
+
+        protected override void OnStop()
+        {
+            _host.Stop();
+            base.OnStop();
+        }
+    }
+}
