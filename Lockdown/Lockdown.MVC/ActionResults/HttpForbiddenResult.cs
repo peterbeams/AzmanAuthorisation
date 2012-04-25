44da846affcea4cc72a97d99c2c08a4db0465c6e
Nick Blair <blair.nicholas@googlemail.com>
2012-04-25 23:07:21 +0100

    HttpForbiddenResult changed to throw error rather than provide 403 response

--- Lockdown/Lockdown.MVC/ActionResults/HttpForbiddenResult.cs
+++ Lockdown/Lockdown.MVC/ActionResults/HttpForbiddenResult.cs
@@ -1,25 +1,14 @@
 ﻿using System;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Lockdown.MVC.ActionResults
 {
     public class HttpForbiddenResult : ContentResult
     {
-        public HttpForbiddenResult()
-        {
-            Content = "403 Forbidden";
-            ContentType = "text/plain";
-        }
-
         public override void ExecuteResult(ControllerContext context)
         {
-            if (context == null)
-            {
-                throw new ArgumentNullException("context");
-            }
-
-            context.HttpContext.Response.StatusCode = 403;
-            base.ExecuteResult(context);
+            throw new HttpException(403, "Forbidden");
         }
     }
 }
