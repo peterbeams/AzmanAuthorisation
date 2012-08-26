5f86159e80119e4ed6df87ef9bfab9d668a77ce4
Peter Beams <peter.beams@googlemail.com>
2012-08-26 21:46:32 +0100

    Adding sample MVC application

---
+++ Lockdown/SampleMVCApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleMVCApplication.MvcApplication" Language="C#" %>
--- Lockdown/SampleMVCApplication/Properties/AssemblyInfo.cs
+++ Lockdown/SampleMVCApplication/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SampleApplication")]
+[assembly: AssemblyTitle("SampleMVCApplication")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SampleApplication")]
+[assembly: AssemblyProduct("SampleMVCApplication")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e717b1ce-091c-4809-86fd-d8ae8516f6cc")]
+[assembly: Guid("90c7a401-8c40-42d8-a1a2-3d6b09c4a768")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
