4f2fb25d0e18110e01b3c2b88a642f64830b9562
Peter Beams <peter.beams@googlemail.com>
2013-02-19 20:34:13 +0000

    Allowing support for multiple apps

--- Lockdown/Lockdown.Host/AzmanAuthzService.cs
+++ Lockdown/Lockdown.Host/AzmanAuthzService.cs
@@ -77,7 +77,7 @@ namespace Lockdown.Host
             {
                 Store.UsingApplication(appName);
 
-                var opNames = Store.GetAuthroizedOperations(token.Sids);
+                var opNames = Store.GetAuthroizedOperations(appName, token.Sids);
                 return new AuthorizedOperations { OperationNames = opNames };
             }
             catch (Exception ex)
@@ -94,7 +94,7 @@ namespace Lockdown.Host
             try
             {
                 Store.UsingApplication(appName);
-                Array.ForEach(operationNames, o => Store.EnsureOperationByName(o));
+                Array.ForEach(operationNames, o => Store.EnsureOperationByName(appName, o));
             }
             catch (Exception ex)
             {
@@ -111,7 +111,7 @@ namespace Lockdown.Host
             {
                 Store.UsingApplication(appName);
 
-                var opNames = Store.GetRoles().Where(r => r.Members.Any(m => token.Sids.Contains(m.Id)) );
+                var opNames = Store.GetRoles(appName).Where(r => r.Members.Any(m => token.Sids.Contains(m.Id)) );
                 return opNames.Select(r => r.Name).ToArray();
             }
             catch (Exception ex)
